91577d136de92269c1e84e0d4747953be981ebad
Dominique Lasserre <lasserre.d@gmail.com>
2016-04-05 09:56:22 +0200

    License headers

--- rng.h
+++ rng.h
@@ -1,3 +1,23 @@
+/**
+ * goiovalues - Guns of Icarus Online damage simulator
+ * Copyright (C) 2016  Dominique Lasserre
+ *
+ * This file is part of goiovalues.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef RNG_H_
 #define RNG_H_
 
@@ -12,7 +32,7 @@ extern std::function<double ()> percentage;
 inline void init() {
   std::random_device rd;
   std::mt19937 rng(rd());
-  std::uniform_real_distribution<double> uni(0,100);
+  std::uniform_real_distribution<double> uni(0, 100);
 
   percentage = std::bind(uni, rng);
 }
